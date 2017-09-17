870594aa447e80329bd5270930bc745e8b94bee9
yesI <1317154549@qq.com>
2017-09-17 02:31:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,3 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
-
-if (name === 'Susan') {
-  console.log('the name is correct')
-}else {
-  console.log('hope the name to be Susan');
-}
